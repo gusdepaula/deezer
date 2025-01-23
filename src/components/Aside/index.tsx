dc98b79a70f86d0cf016a6c459a0a1c06060f71b
Gustavo de Paula <gdepaula@gmail.com>
2025-01-23 11:42:39 -0300

    Add web app manifest and update HTML for improved PWA support; include favicon and apple touch icon

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -9,7 +9,7 @@ export default function Aside({ resetTracks }: AsideProps) {
   return (
     <Box as="aside" width={{ base: '100%', md: '20%' }} p="4" height={{ base: 'auto', md: '100vh' }}>
       <Link to={'/'} onClick={resetTracks}>
-        <Image src="../../../public/images/logo-deezer.png" alt="Deezer GusdePaula" mx="auto" my={{ base: '2', md: '4' }} />
+        <Image src="../../../public/images/logo-deezer.png" alt="Deezer GusDePaula" mx="auto" my={{ base: '2', md: '4' }} />
       </Link>
       <Link to={'/favorites'} onClick={resetTracks}>
         Favorites
