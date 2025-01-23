5905073f80d5a7cd070428849e2b6ddf17eacf20
Gustavo de Paula <gdepaula@gmail.com>
2025-01-23 20:22:13 -0300

    Refactor API calls to use relative paths; add Vercel rewrites for proxying requests to Deezer API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://api.deezer.com';
-
 export const fetchTopTracks = async () => {
-  const response = await axios.get(`${API_BASE_URL}/chart/0/tracks`);
+  const response = await axios.get('/api/chart/0/tracks');
   return response.data.data;
 };
 
 export const searchTracks = async (query: string) => {
-  const response = await axios.get(`${API_BASE_URL}/search?q=${query}`);
+  const response = await axios.get(`/api/search?q=${query}`);
   return response.data.data;
 };
