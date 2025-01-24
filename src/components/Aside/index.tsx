f56f3673408aaacab87d2f4f1f2d6683f025a7c6
Gustavo de Paula <gdepaula@gmail.com>
2025-01-24 16:19:01 -0300

    Add Home button to Aside component and improve layout

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,6 +1,6 @@
 import { AsideProps } from '../../types';
 import { Box, Button, Image, ButtonProps } from '@chakra-ui/react';
-import { FaHeart } from 'react-icons/fa';
+import { FaHeart, FaHome } from 'react-icons/fa';
 import { Link, LinkProps } from 'react-router-dom';
 
 const ButtonLink = (props: ButtonProps & LinkProps) => <Button as={Link} {...props} />;
@@ -12,15 +12,28 @@ const Aside = ({ resetTracks }: AsideProps) => {
         <Image src="/images/logo-deezer.png" alt="Deezer GusDePaula" mx="auto" my={{ base: '2', md: '4' }} />
       </Link>
 
-      <Box display={{ base: 'flex', md: 'block' }} justifyContent="center" mt="4">
+      <Box display="flex" flexDirection="column" alignItems="center" mt="4">
+        <ButtonLink
+          to="/"
+          onClick={resetTracks}
+          colorScheme="teal"
+          size={{ base: 'md', md: 'sm' }}
+          width="100%"
+          _hover={{ transform: 'translateY(-2px)', boxShadow: 'lg' }}
+          mb="4"
+        >
+          <FaHome style={{ marginRight: '8px' }} />
+          Home
+        </ButtonLink>
         <ButtonLink
           to="/favorites"
           onClick={resetTracks}
           colorScheme="teal"
           size={{ base: 'md', md: 'sm' }}
+          width="100%"
           _hover={{ transform: 'translateY(-2px)', boxShadow: 'lg' }}
         >
-          <FaHeart />
+          <FaHeart style={{ marginRight: '8px' }} />
           Favorites
         </ButtonLink>
       </Box>
