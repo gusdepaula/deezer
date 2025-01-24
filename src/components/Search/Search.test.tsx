c18283e94ef30172d45bc745035c53338cacbf63
Gustavo de Paula <gdepaula@gmail.com>
2025-01-24 11:42:40 -0300

    Refactor Search component test to use mock props for improved readability and maintainability

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -1,15 +1,23 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import Search from './index';
+import { SearchProps } from '../../types';
 
-const mockOnSearch = vi.fn();
+const mockSetTracks = vi.fn();
+const mockSetSearchTerm = vi.fn();
+
+const mockProps: SearchProps = {
+  setTracks: mockSetTracks,
+  searchTerm: 'Foo Fighters',
+  setSearchTerm: mockSetSearchTerm,
+};
 
 describe('Search', () => {
   it('renderiza o campo de entrada e o botão de busca', () => {
     render(
       <ChakraProvider value={defaultSystem}>
-        <Search setTracks={mockOnSearch} searchTerm={mockOnSearch} setSearchTerm={mockOnSearch} />
+        <Search {...mockProps} />
       </ChakraProvider>,
     );
 
