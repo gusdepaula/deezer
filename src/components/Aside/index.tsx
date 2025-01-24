f337dbe277a5d36c2699cfec4fdf60e3d9315224
Gustavo de Paula <gdepaula@gmail.com>
2025-01-24 15:34:17 -0300

    Refactor Aside component to use Link instead of RouterLink for improved consistency

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,16 +1,16 @@
 import { AsideProps } from '../../types';
 import { Box, Button, Image, ButtonProps } from '@chakra-ui/react';
 import { FaHeart } from 'react-icons/fa';
-import { Link as RouterLink, LinkProps } from 'react-router-dom';
+import { Link, LinkProps } from 'react-router-dom';
 
-const ButtonLink = (props: ButtonProps & LinkProps) => <Button as={RouterLink} {...props} />;
+const ButtonLink = (props: ButtonProps & LinkProps) => <Button as={Link} {...props} />;
 
 const Aside = ({ resetTracks }: AsideProps) => {
   return (
     <Box as="aside" width={{ base: '100%', md: '10%' }} p="4" height={{ base: 'auto', md: '100vh' }}>
-      <RouterLink to={'/'} onClick={resetTracks}>
+      <Link to={'/'} onClick={resetTracks}>
         <Image src="/images/logo-deezer.png" alt="Deezer GusDePaula" mx="auto" my={{ base: '2', md: '4' }} />
-      </RouterLink>
+      </Link>
 
       <Box display={{ base: 'flex', md: 'block' }} justifyContent="center" mt="4">
         <ButtonLink
@@ -20,7 +20,7 @@ const Aside = ({ resetTracks }: AsideProps) => {
           size={{ base: 'md', md: 'sm' }}
           _hover={{ transform: 'translateY(-2px)', boxShadow: 'lg' }}
         >
-          <FaHeart style={{ marginRight: '8px' }} />
+          <FaHeart />
           Favorites
         </ButtonLink>
       </Box>
