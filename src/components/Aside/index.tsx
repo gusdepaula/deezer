3f45d52d388e429a7734ceaba4819b71baee9df8
Gustavo de Paula <gdepaula@gmail.com>
2025-01-29 16:06:36 -0300

    Update Aside and ButtonFavorite components for color mode support; adjust logo and heart icon colors

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -2,16 +2,23 @@ import { AsideProps } from '../../types';
 import { Box, Button, Image, ButtonProps } from '@chakra-ui/react';
 import { FaHeart, FaHome } from 'react-icons/fa';
 import { Link, LinkProps, useLocation } from 'react-router-dom';
+import { useColorMode } from '../ui/color-mode';
 
 const ButtonLink = (props: ButtonProps & LinkProps) => <Button as={Link} {...props} />;
 
 const Aside = ({ resetTracks }: AsideProps) => {
   const location = useLocation();
+  const { colorMode } = useColorMode();
 
   return (
     <Box as="aside" width={{ base: '100%', md: '10%' }} p="4" height={{ base: 'auto', md: '100vh' }}>
       <Link to={'/'} onClick={resetTracks}>
-        <Image src="/images/logo-deezer.png" alt="Deezer GusDePaula" mx="auto" my={{ base: '2', md: '4' }} />
+        <Image
+          src={colorMode === 'light' ? '/images/logo-deezer-light.png' : '/images/logo-deezer-dark.png'}
+          alt="Deezer GusDePaula"
+          mx="auto"
+          my={{ base: '2', md: '4' }}
+        />
       </Link>
 
       <Box display="flex" flexDirection="column" alignItems="center" mt="4">
--- src/components/ButtonFavorite/index.tsx
+++ src/components/ButtonFavorite/index.tsx
@@ -1,17 +1,16 @@
 import { Button } from '@chakra-ui/react';
 import { FaHeart } from 'react-icons/fa';
 import { ButtonFavoriteProps } from '../../types';
+import { useColorMode } from '../ui/color-mode';
 
 const ButtonFavorite = ({ track, favorites, setFavorites }: ButtonFavoriteProps) => {
+  const { colorMode } = useColorMode(); // Extraindo a propriedade correta
   const isFavorite = favorites.some(favorite => favorite.id === track.id);
+  const colorHeart = isFavorite ? 'red' : colorMode === 'light' ? 'white' : 'black';
 
   const handleAddToFavorites = () => {
-    let updatedFavorites;
-    if (isFavorite) {
-      updatedFavorites = favorites.filter(favorite => favorite.id !== track.id);
-    } else {
-      updatedFavorites = [...favorites, track];
-    }
+    let updatedFavorites = isFavorite ? favorites.filter(favorite => favorite.id !== track.id) : [...favorites, track];
+
     setFavorites(updatedFavorites);
     localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
     console.log(`Atualizando favoritos:`, updatedFavorites);
@@ -19,7 +18,7 @@ const ButtonFavorite = ({ track, favorites, setFavorites }: ButtonFavoriteProps)
 
   return (
     <Button onClick={handleAddToFavorites} colorScheme="teal" size="sm" _hover={{ transform: 'translateY(-2px)', boxShadow: 'lg' }}>
-      <FaHeart style={{ fill: isFavorite ? 'red' : 'black' }} />
+      <FaHeart style={{ fill: colorHeart }} />
     </Button>
   );
 };
